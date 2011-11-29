9b0e422f76e45bb6295443d3897cda5ce8b0bb20
Eric Kelly <heroiceric@gmail.com>
2011-11-29 15:37:34 -0500

    Small change to hopefully fix the problem with heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    @update = current_user.updates.new unless current_user.nil?
+    @update = current_user.updates.new
   end
 end
