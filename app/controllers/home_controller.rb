0b5d2595b8a71e94e18bde56cfebe10084e10417
Eric Kelly <heroiceric@gmail.com>
2011-11-29 16:24:23 -0500

    Small change to hopefully fix the problem with heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    @updates = current_user.updates.new if user_signed_in?
+    @update = current_user.updates.new if user_signed_in?
   end
 end
