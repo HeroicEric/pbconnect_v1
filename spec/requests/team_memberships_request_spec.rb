6282f1b1beef4bcef7d7683aecba8881f5414a66
Eric Kelly <heroiceric@gmail.com>
2012-01-04 17:59:16 -0500

    Some styling changes and refactoring

--- spec/requests/team_memberships_request_spec.rb
+++ spec/requests/team_memberships_request_spec.rb
@@ -2,46 +2,48 @@ require 'spec_helper'
 
 describe "team_memberships request" do
 
-  describe "GET /team" do
-    before(:each) do
-      @team = Factory(:team)
-      @eric_admin = Factory(:user, name: "Eric Kelly")
-      @fred = Factory(:user, name: "Fred Flinstone")
-      RosterAddition.new(team: @team, member: @eric_admin, role: 'admin')
-      login_user(@eric_admin)
-      visit team_path(@team)
-    end
+  before(:each) do
+    @team = Factory(:team)
+    @admin = Factory(:user)
+    @player = Factory(:user)
+    @team.add_admin(@admin)
+    login_user(@admin)
+    visit team_path(@team)
+  end
 
-    it "only gives the option to add users who on the team" do
+  describe "GET /team" do
+    it "gives the option to add users who are not on the team" do
       within "#team_membership_user_id" do
-        page.should_not have_css("option[@value='#{@eric_admin.id}']")
-        page.should have_css("option[@value='#{@fred.id}']")
+        page.should_not have_css("option[@value='#{@admin.id}']")
+        page.should have_css("option[@value='#{@player.id}']")
       end
+    end
+  end
+
+  describe "POST /team" do
+    it "fails if the current_user is not authorized" do
+      select @player.name, from: "team_membership[user_id]"
+      click_button "Add Member"
     end
 
     describe "success" do
       it "adds a member with role of player" do
-        select @fred.name, from: "team_membership[user_id]"
+        select @player.name, from: "team_membership[user_id]"
         click_button "Add Member"
         current_path.should == team_path(@team)
-        page.should have_content "#{@fred.name} was successfully added to the roster!"
+        page.should have_content "#{@player.name} was successfully added to the roster!"
         within("#roster .members") do
-          page.should have_content @fred.name
+          page.should have_content @player.name
         end
-        @team.role_of(@fred).should == "player"
+        @team.role_of(@player).should == "player"
       end
     end
   end
 
   describe "DELETE /team_membership" do
-    before(:each) do
-      @team = Factory(:team)
-      @player = Factory(:user)
-      @team_membership = TeamMembership.create!(team_id: @team.id, user_id: @player.id, role: 'player')
-      visit team_path(@team)
-    end
-
     it "deletes the team_membership of a player" do
+      @team.add_player(@player)
+      visit team_path(@team)
       within "#member-#{@player.id}" do
         find("input[@rel='delete-team-membership']").click
       end
