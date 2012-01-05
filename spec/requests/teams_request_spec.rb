6790327a9856a4c0a3db547ddaf7085cc4d7be19
Eric Kelly <heroiceric@gmail.com>
2012-01-05 18:25:23 -0500

    Some refactoring of tests

--- spec/requests/teams_request_spec.rb
+++ spec/requests/teams_request_spec.rb
@@ -2,6 +2,59 @@ require 'spec_helper'
 
 describe "teams request" do
 
+  describe "GET /teams" do
+    it "lists links to all of the teams' show pages" do
+      @teams = []
+      5.times{ @teams << Factory(:team) }
+      visit teams_path
+      @teams.each do |team|
+        page.should have_link(team.name, href: team_path(team))
+      end
+    end
+
+    context "there are no teams in the database" do
+      it "tells the user that there are no teams yet" do
+        Team.all.should be_empty
+        visit teams_path
+        page.should have_content "There are no teams yet."
+      end
+    end
+  end
+
+  describe "GET /team" do
+    before(:each) do
+      @team = Factory(:team)
+      3.times{ RosterAddition.new(team: @team, member: Factory(:user)) }
+      visit team_path(@team)
+    end
+
+    it "lists the members with links to their profiles" do
+      @team.members.each do |member|
+        page.should have_link(member.name, href: user_path(member))
+      end
+    end
+
+    context "user is admin" do
+      it "shows a remove member button for each member" do
+        @team.active_users.each do |user|
+          within("#member-#{user.id}") do
+            page.should have_css("input[@rel='delete-team-membership']")
+          end
+        end
+      end
+
+      it "shows a 'make admin' button for each non-admin" do
+        @team.active_users.each do |user|
+          if @team.is_admin?(user) then
+            page.should_not have_css("#member-#{user.id} input[@rel='make-admin']")
+          else
+            page.should have_css("#member-#{user.id} input[@rel='make-admin']")
+          end
+        end
+      end
+    end
+  end
+
   describe "POST /team" do
     context "an authorized user" do
       before(:each) do
@@ -25,79 +78,6 @@ describe "teams request" do
         team = Team.find_by_name("Dynasty")
         team.members.where("team_memberships.role = 'admin'").should include(current_user)
       end
-    end
-  end
-
-  describe "GET /teams" do
-    context "on the team index page" do
-      context "if there are teams in the database" do
-        it "lists links to all of the teams' show pages" do
-          @teams = []
-          5.times{ @teams << Factory(:team) }
-          visit teams_path
-          @teams.each do |team|
-            page.should have_link(team.name, href: team_path(team))
-          end
-        end
-      end
-
-      context "there are no teams in the database" do
-        it "displays a descriptive message" do
-          Team.all.should be_empty
-          visit teams_path
-          page.should have_content "There are no teams yet."
-        end
-      end
-    end
-  end
-
-  describe "GET /team" do
-    context "for everybody" do
-      before(:each) do
-        @team = Factory(:team)
-        3.times{ RosterAddition.new(team: @team, member: Factory(:user)) }
-        visit team_path(@team)
-      end
-
-      it "lists the members" do
-        @team.members.each do |member|
-          page.should have_content member.name
-        end
-      end
-
-      it "lists links to all players profiles" do
-        @team.members.each do |member|
-          page.should have_link(member.name, href: user_path(member))
-        end
-      end
-
-      context "user is admin" do
-        describe "showing the admninistrative actions" do
-
-          describe "remove member button" do
-            it "shows the remove member button" do
-              @team.active_users.each do |user|
-                within("#member-#{user.id}") do
-                  page.should have_css("input[@rel='delete-team-membership']")
-                end
-              end
-            end
-          end
-
-          it "shows the 'make admin' button for users that are not admins" do
-            @team.active_users.each do |user|
-              within("#member-#{user.id}") do
-                if @team.is_admin?(user)
-                  page.should_not have_css("input[@rel='make-admin']")
-                else
-                  page.should have_css("input[@rel='make-admin']")
-                end
-              end
-            end
-          end
-        end
-      end
-
     end
   end
 end
